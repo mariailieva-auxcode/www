1da026c9a4fe7440a166c8e7c9b1c37d23e9480c
Milen Markov <m1@auxcode.com>
2020-06-11 19:50:52 +0300

    Added table with supliers

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,3 +1,51 @@
 <template>
-    <div><router-link to="/">Go to News</router-link></div>
+  <div class="supliers">
+    <router-link to="/">Go to News</router-link>
+    <!-- <div class="w-50 p-3">
+      <input type="number" placeholder="number filter" />
+    </div>-->
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Max Area Filter</span>
+      </div>
+      <input type="number" class="form-control" aria-label="Max Area" @input="filteredSupliers" />
+    </div>
+    <div>
+      <table class="table" border="2px">
+        <thead>
+          <th>Company Name</th>
+          <th>Area</th>
+        </thead>
+        <tbody>
+          <tr v-for="(suplier, key, index) in filteredSupliers" :key="index">
+            <td>{{suplier.companyName}}</td>
+            <td>{{suplier.area}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<script>
+import info from "js-yaml-loader!../../supliers.yaml";
+export default {
+  name: "Supliers",
+
+  data() {
+    return {
+      supliers: [],
+      filteredSupliers: []
+    };
+  },
+  beforeMount() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.supliers = info;
+      this.filteredSupliers = info;
+    }
+  }
+};
+</script>
