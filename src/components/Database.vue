9bd95fde71dc0c95804883701da0cc2962342ebd
Milen Markov <m1@auxcode.com>
2020-07-04 22:50:23 +0300

    Fixed component Database design

--- src/components/Database.vue
+++ src/components/Database.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="database">
     <div class="row">
-      <p class="two">02</p>
       <h1>Database</h1>
     </div>
     <div class="row">
       <div class="col-6">
         <img src="../../public/assets/person-writing-on-notebook.png" />
       </div>
-      <div class="col-6">
+      <div class="col-6 text">
         <p>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
           tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
           At vero eos et accusam et justo duo dolores et ea rebum. Stet clita.
         </p>
-        <button>Coming soon</button>
+        <button>
+          <p>
+            Coming soon
+            <img src="../../public/assets/arrow-right-blue.png" />
+          </p>
+        </button>
       </div>
     </div>
   </div>
@@ -29,13 +33,32 @@ export default {};
 .database {
   .row {
     max-width: 80%;
-    .two {
-      z-index: 0;
-      font-size: 190px;
-      color: #f7f7fa;
-    }
     h1 {
-      margin-top: 150px;
+      margin: 150px 0 80px 80px;
+    }
+    .col-6 {
+      p {
+        text-align: center;
+        font-size: 18px;
+        color: #9597ac;
+        margin-bottom: 50px;
+      }
+    }
+    .text {
+      margin-top: auto;
+      margin-bottom: auto;
+      button {
+        width: 140px;
+        height: 45px;
+        border-radius: 10px;
+        border: 2px solid #2783ff;
+        background-color: white;
+        p {
+          color: #2783ff;
+          font-size: 14px;
+          margin: auto 0;
+        }
+      }
     }
   }
 }
