24423500b73fd4ec4accceec4d59fab119608783
Milen Markov <m1@auxcode.com>
2020-07-05 10:26:04 +0300

    Fixed Team component design

--- src/components/OnsTeam.vue
+++ src/components/OnsTeam.vue
@@ -4,30 +4,37 @@
       <h1>Ons team</h1>
     </div>
     <div class="row yaml-links row" v-for="team of team" :key="team">
-      <div class="col-3">
+      <div class="col-3 picture">
         <img :src="team.picture" />
       </div>
       <div class="col-9">
-        <div class="col-12">
+        <div class="col-12 name">
           <h3>{{team.name}}</h3>
         </div>
 
         <div class="row">
-          <div class="col-12">
+          <div class="col-12 position">
             <p>{{team.position}}</p>
           </div>
         </div>
         <div class="row">
-          <div class="col-12">
+          <div class="col-12 about">
             <p>{{team.about}}</p>
           </div>
         </div>
-        <div class="row">
+        <div class="row information">
           <div class="col-1">
             <img src="../../public/assets/envelope.png" />
           </div>
-          <div class="col-11">
+          <div class="col-6 email">
             <p>{{team.email}}</p>
+          </div>
+          <div class="col-5 linkedin">
+            <div class="row">
+              <img class="linkedin" src="../../public/assets/linked-in-blue.png" />
+              <p class="email">Visit LinkedIn profile</p>
+              <img class="arrow" src="../../public/assets/arrow-right-blue.png" />
+            </div>
           </div>
         </div>
       </div>
@@ -53,7 +60,7 @@ export default {
     this.name = team.name;
     this.position = team.position;
     this.about = team.about;
-    this.email - team.email;
+    this.email = team.email;
   }
 };
 </script>
@@ -75,6 +82,42 @@ export default {
     box-shadow: 0px 0px 30px #1d226f1a;
     border-radius: 10px;
     text-align: left;
+    .picture {
+      padding-left: 0;
+    }
+  }
+  .col-9 {
+    .name {
+      font-size: 18px;
+      color: #26272e;
+      font-weight: bold;
+    }
+    .position {
+      font-size: 16px;
+      font-weight: bold;
+      color: #9597ac;
+    }
+    .about {
+      font-size: 16px;
+      color: #9597ac;
+    }
+    .email {
+      font-size: 14px;
+      color: #5694e8;
+    }
+    .col-5 {
+      .linkedin {
+        width: 20px;
+        height: 20px;
+        margin-right: 10px;
+      }
+      .arrow {
+        width: 12px;
+        height: 12px;
+        margin-left: 10px;
+        margin-top: 5px;
+      }
+    }
   }
 }
 </style>
