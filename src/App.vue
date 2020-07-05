8cdbe72497a264aa6bc3f7f08596fa6043285c6c
Radoslav Radev <radoslav@auxcode.com>
2020-07-05 16:32:44 +0300

    fixed news design

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "./assets/styles/main.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
