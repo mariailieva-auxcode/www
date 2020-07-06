1566a42a425533f48f6309257babb1338ea69644
Milen Markov <m1@auxcode.com>
2020-07-06 19:28:05 +0300

    Fixed Cookie design

--- src/App.vue
+++ src/App.vue
@@ -8,36 +8,47 @@
       <Footer></Footer>
     </div>
 
-    <cookie-law>
+    <cookie-law class="cookie">
       <div slot-scope="props">
-        <button class="skew" @click="props.accept">
-          <span>I accept</span>
-        </button>
-        <p>This site uses cookies 🍪</p>
-        <button class="skew" @click="props.close">
+        <div class="row">
+          <img src="../public/assets/cookie.png" />
+          <p>
+            This site uses
+            <span class="undeline">cookies</span>
+          </p>
+        </div>
+        <div class="row">
+          <button class="skew" @click="props.accept">
+            <span>OK</span>
+          </button>
+        </div>
+
+        <!-- <button class="skew" @click="props.close">
           <span>Ignore me</span>
-        </button>
+        </button>-->
       </div>
     </cookie-law>
   </div>
 </template>
 
 <script>
-import Navigation from "./components/layout/Navigation.vue"
+import Navigation from "./components/layout/Navigation.vue";
 import Footer from "./components/layout/Footer.vue";
 import CookieLaw from "vue-cookie-law";
-
 
 export default {
   name: "App",
   components: {
-    Navigation, Footer, CookieLaw
+    Navigation,
+    Footer,
+    CookieLaw
   }
 };
 </script>
 
 <style lang="scss">
 @import "./assets/styles/main.scss";
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -46,9 +57,39 @@ export default {
   color: #2c3e50;
 }
 
-
 .content {
   width: calc(100vw - 100px);
   float: right;
+}
+
+.cookie {
+  width: 340px;
+  height: 170px;
+  justify-content: center;
+  img {
+    margin-right: 20px;
+  }
+  p {
+    font-size: 14px;
+    color: #26272e;
+    font-family: "Rubik", sans-serif;
+    margin-top: 20px;
+    span {
+      text-decoration: underline;
+    }
+  }
+  button {
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #ffc800;
+    border-radius: 10px;
+    border: none;
+    padding: 13px 70px;
+    margin-top: 20px;
+    span {
+      font-family: "Rubik", sans-serif;
+      font-size: 14px;
+    }
+  }
 }
 </style>
