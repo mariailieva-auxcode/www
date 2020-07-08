05d808654020cfe663b692c6d7b60ccfd67ddff5
Milen Markov <m1@auxcode.com>
2020-07-08 19:11:54 +0300

    Added border line for indicates which page is open

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -6,40 +6,44 @@
       </router-link>
     </div>
     <div class="row">
-      <div class="col-12 home-logo">
-        <router-link :to="`/?lang=${$router.history.current.query.lang}`">
+      <div class="row">
+        <div class="col-12 home-logo">
+          <router-link :to="`/?lang=${$router.history.current.query.lang}`">
+            <div v-if="currentPage === '/'" class="green-line"></div>
+            <button>
+              <img :src="currentPage === '/' ? 'assets/home.svg' : 'assets/home-alt.svg'" />
+              <p class="home">Home</p>
+            </button>
+          </router-link>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 news-logo">
+          <router-link :to="`/news?lang=${$router.history.current.query.lang}`">
+            <div v-if="currentPage === '/news'" class="green-line"></div>
+            <button>
+              <img
+                :src="currentPage === '/news' ? 'assets/newspaper.svg' : 'assets/newspaper-alt.svg'"
+              />
+              <p class="news">News</p>
+            </button>
+          </router-link>
+        </div>
+      </div>
+      <div class="row ml">
+        <router-link to="?lang=nl">
           <button>
-            <img :src="currentPage === '/' ? 'assets/home.svg' : 'assets/home-alt.svg'" />
-            <p class="home">Home</p>
+            <p>NL</p>
           </button>
         </router-link>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12 news-logo">
-        <router-link :to="`/news?lang=${$router.history.current.query.lang}`">
-          <button>
-            <img
-              :src="currentPage === '/news' ? 'assets/newspaper.svg' : 'assets/newspaper-alt.svg'"
-            />
-            <p class="news">News</p>
-          </button>
-        </router-link>
-      </div>
-    </div>
-    <div class="row ml">
-      <router-link to="?lang=nl">
-        <button>
-          <p>NL</p>
-        </button>
-      </router-link>
 
-      <div class="line"></div>
-      <router-link to="?lang=en">
-        <button>
-          <p>EN</p>
-        </button>
-      </router-link>
+        <div class="line"></div>
+        <router-link to="?lang=en">
+          <button>
+            <p>EN</p>
+          </button>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -77,12 +81,14 @@ export default {
     }
     button {
       border: none;
+      margin-top: 10px;
       background-color: white;
       .news,
       .home {
         color: #9597ac;
         font-size: 14px;
         margin-top: 5px;
+        margin-bottom: 0;
         font-weight: bold;
       }
       &:focus {
@@ -105,8 +111,16 @@ export default {
       height: 1px;
       border-bottom: 15px solid #afafaf;
       border-radius: 10px;
-      margin-top: 5px;
+      margin-top: 10px;
     }
   }
+}
+.green-line {
+  width: 5px;
+  height: 1px;
+  border-bottom: 70px solid #55b364;
+  border-radius: 10px;
+  position: absolute;
+  left: -10px;
 }
 </style>
