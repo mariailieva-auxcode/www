34595ba089120f06c751f6ce95c59dd4eb721567
Milen Markov <m1@auxcode.com>
2020-08-08 17:29:18 +0300

    Changed build direction

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://development-functions-test.netlify.app/.netlify/functions/'
+const API_URL = 'https://gracious-hodgkin-e6efd3.netlify.app/.netlify/functions/'
 // const API_URL = "http://localhost:9000/"
 
 export default axios.create({
