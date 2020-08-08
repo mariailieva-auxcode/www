d7b27353a3b2d9cb6673da787ef2d2a8ad082e8d
Peter Cat <peter@auxnederlandbv.nl>
2020-08-08 14:48:26 +0300

    add initial setup

---
+++ functions/test.ts
@@ -0,0 +1,3 @@
+export function alo(s: string): string {
+    return "haha";
+}
---
+++ interface.ts
@@ -0,0 +1,7 @@
+import { alo } from './functions/test';
+
+// var message:string = "Hello World" 
+console.log(alo(""));
+
+
+
