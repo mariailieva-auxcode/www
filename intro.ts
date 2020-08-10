3902e58b0515a81362f17fe743c2f433c3b55dcf
Peter Cat <peter@auxnederlandbv.nl>
2020-08-10 13:06:31 +0300

    rename interface.ts to intro.ts

---
+++ intro.ts
@@ -0,0 +1,27 @@
+import { Archetype } from "./functions/archetypes";
+import {
+    Input,
+    getConditionsFromInput
+} from "./functions/conditions";
+
+import * as sdeTariffsData from "./data/SDETariffsCategory.json";
+
+
+const INPUT: Input = {
+    InvestmentYear: 2020,
+    YearOfProduction: 2021,
+    Capacity: 1000,
+    Yield: 950,
+    DirectOwnConsumption: 0.00,
+    LandCosts: 44000,
+    LandArea: 200,
+    LandRentCosts: 6000,
+    RentingTheLand: true,
+    archetype: Archetype.GroundMountedProducer,
+}
+
+let result = getConditionsFromInput(INPUT, sdeTariffsData);
+console.log(result);
+
+
+
