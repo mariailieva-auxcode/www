8a0d6ba40549ff4ca0b1db071bcd0a56f051245b
Peter Cat <peter@auxnederlandbv.nl>
2020-08-10 10:49:35 +0300

    add input to interface

--- interface.ts
+++ interface.ts
@@ -1,7 +1,23 @@
-import { alo } from './functions/test';
+import { Archetype } from "./functions/archetypes";
 
-// var message:string = "Hello World" 
-console.log(alo(""));
+// Input data
+const INPUT = {
+    InvestmentYear : 2020,
+    YearOfProduction : 2021,
+    Capacity : 1000, // (kWp)
+    Yield : 950, // (kWh/kWp)
+    DirectOwnConsumption : 0.00, // (%)
+    LandCosts : 44000, // (€/hectere)
+    LandArea : 200, // (m2)
+    LandRentCosts : 6000, // (€/hectere/year)
+    RentingTheLand : true,
+    archetype: Archetype.RoofMountedProsumer,
+}
+
+
+
+
+
 
 
 
