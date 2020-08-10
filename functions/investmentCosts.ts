b9060831f1f0d3dd6ad63ecbaafa41af04e99331
Peter Cat <peter@auxnederlandbv.nl>
2020-08-10 18:48:27 +0300

    add investment costs model

---
+++ functions/investmentCosts.ts
@@ -0,0 +1,87 @@
+export function generateInstallationSizes(size: number = 214): number[] {
+    let installationSizes = [15]; // init as a param?
+    for (let i = 1; i < size; i++) {
+        installationSizes.push(i * 50); // also parametrize?
+    }
+    return installationSizes;
+}
+
+export function generateSolarPanelICs(
+    size: number,
+    range: number,
+    gridConnection: number): number[] {
+    if (size < range) {
+        size = range; // so it makes sense
+    }
+
+    const init = 1049.5867768595 - gridConnection; // where does this come from??
+    const end = 830; // param?
+    let solarPanelICs = [init, init];
+
+    for (let i = 0; i < range - 1; i++) {
+        solarPanelICs.push(end + (init - end) / range * (range - i));
+    }
+    for (let i = 0; i < size - range - 1; i++) {
+        solarPanelICs.push(end);
+    }
+    return solarPanelICs;
+}
+
+export function investmentCost(
+    year: number,
+    solarPanelCost: number,
+    aeCostReduction: number,
+    refYear: number,
+) {
+    return solarPanelCost * Math.pow((1 - aeCostReduction), (year - refYear));
+}
+
+export function generateYearlyCashFlow(
+    beginYear: number,
+    endYear: number,
+    solarPanelIC: number,
+    aeCostReduction: number,
+    refYear: number): number[] {
+    let cashFlow = [];
+    for (let i = beginYear; i <= endYear; i++) {
+
+        cashFlow.push(investmentCost(i, solarPanelIC, aeCostReduction, refYear))
+    }
+    return cashFlow;
+}
+
+export interface InvestmentCostTableRow {
+    SolarPanelsInvestmentCost: number,
+    installationSize: number,
+    beginYear: number,
+    endYear: number,
+    yearlyCashFlow: number[]
+}
+
+export function generateInvestmentCostTable(
+    size: number,
+    range: number,
+    gridConnection: number,
+    beginYear: number,
+    endYear: number,
+    aeCostReduction: number,
+    refYear: number): InvestmentCostTableRow[] {
+    let solarPanelICs = generateSolarPanelICs(size, range, gridConnection);
+    let installationSizes = generateInstallationSizes(size);
+
+    return solarPanelICs.map((v, i) => {
+        return {
+            SolarPanelsInvestmentCost: v,
+            installationSize: installationSizes[i],
+            beginYear,
+            endYear,
+            yearlyCashFlow: generateYearlyCashFlow(
+                beginYear,
+                endYear,
+                v,
+                aeCostReduction,
+                refYear
+            )
+        };
+    });
+}
--- intro.ts
+++ intro.ts
@@ -1,8 +1,9 @@
 import { Archetype } from "./models/archetypes";
 import { Input, getConditionsFromInput } from "./models/conditions";
+import { generateInvestmentCostTable } from "./functions/investmentCosts";
 
 import * as sdeTariffsData from "./data/SDETariffsCategory.json";
-
+import * as investmentCosts from "./data/InvestmentCosts.json";
 
 const INPUT: Input = {
     InvestmentYear: 2020,
@@ -15,10 +16,22 @@ const INPUT: Input = {
     LandRentCosts: 6000,
     RentingTheLand: true,
     archetype: Archetype.GroundMountedProducer,
-}
+};
 
-let result = getConditionsFromInput(INPUT, sdeTariffsData);
-console.log(result);
+let conditions = getConditionsFromInput(INPUT, sdeTariffsData);
+
+let investmentCostsTable = generateInvestmentCostTable(
+    214,
+    30,
+    investmentCosts.GridConnection, INPUT.InvestmentYear - 5,
+    INPUT.InvestmentYear + 10,
+    investmentCosts.AnnualExpectedCostReduction,
+    investmentCosts.ReferenceYear
+);
+
+
+
+
 
 
 
