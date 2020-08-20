4f0b2657799e17a5ca374804f0774b73cbbdd4db
Radoslav Radev <radoslav@auxcode.com>
2020-08-20 23:17:02 +0200

    use .env in functions

--- src-functions/register.js
+++ src-functions/register.js
@@ -1,56 +1,42 @@
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-}
+import { headers } from './constants/headers.constant';
 
 const faunadb = require('faunadb');
 const passwordHash = require('password-hash');
 const q = faunadb.query;
 
-exports.handler = async (event, context, callback) => {
+export async function handler(event, _) {
     try {
         if (event.httpMethod == "POST") {
             /* configure faunaDB Client with our secret */
             console.log("CONNECTING TO DB")
             const client = new faunadb.Client({
-                secret: process.env.FAUNA_SECRET
+                secret: process.env.VUE_APP_FAUNA_SECRET
             })
 
             const data = JSON.parse(event.body)
             data.password = passwordHash.generate(data.password);
 
-            return client.query(q.Create(q.Collection('users'), {
-                    data
-                }))
-                .then((response) => {
-                    console.log('success', response)
-                    callback(null, {
-                        statusCode: 200,
-                        body: JSON.stringify(response),
-                        headers
-                    })
-                }).catch((error) => {
-                    console.log('error', error)
-                    callback(null, {
-                        statusCode: 400,
-                        body: JSON.stringify(error),
-                        headers
-                    })
-                })
+            let response = await client.query(q.Create(q.Collection('users'), {
+                data
+            }))
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response),
+                headers
+            }
         } else {
-            callback(null, {
+            return {
                 statusCode: 204,
                 body: JSON.stringify({}),
                 headers
-            })
+            }
         }
     } catch (error) {
-        callback(null, {
+        return {
             statusCode: 500,
             body: JSON.stringify({
                 status: `error`
             }),
-        });
+        };
     }
 };
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_URL = 'https://gracious-hodgkin-e6efd3.netlify.app/.netlify/functions/'
-const API_URL = process.env.API_URL
+const API_URL = process.env.VUE_APP_API_URL
 
 export default axios.create({
     baseURL: API_URL,
