159ea28262d651c0f48d392c686b5e604168e863
Milen Markov <m1@auxcode.com>
2020-09-18 19:43:12 +0300

    Google map attempt

--- src/components/new-components/Profile.vue
+++ src/components/new-components/Profile.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="profile">
     <button class="logout-button" @click="logout()">Logout</button>
     <div style="width: 100%">
       <iframe
-        width="90%"
+        id="iframe-map"
+        width="100%"
         height="700px"
         frameborder="0"
         scrolling="no"
@@ -18,9 +19,13 @@
 <script>
 export default {
   name: "Profile",
-
   mounted() {
     this.init();
+    setTimeout(() => {
+      console.log(document.getElementsByClassName("place-card"));
+      document.getElementsByClassName("place-card")[0].style.visibility =
+        "hidden";
+    }, 10000);
   },
   watch: {
     lang() {
@@ -40,18 +45,23 @@ export default {
 </script>
 
 <style lang="scss">
-.logout-button {
-  background-color: transparent;
-  border: 2px solid black;
-  border-radius: 10px;
-  margin-left: 20px;
-  font-size: 14px;
-  justify-content: center;
-  color: black;
-  font-weight: 700;
-  width: 148px;
-  height: 46px;
-  align-items: baseline;
-  outline: none;
+.profile {
+  height: 100vh;
+  .logout-button {
+    position: absolute;
+    top: 0;
+    right: 20px;
+    background-color: transparent;
+    border: 2px solid black;
+    border-radius: 10px;
+    font-size: 14px;
+    justify-content: center;
+    color: black;
+    font-weight: 700;
+    width: 148px;
+    height: 46px;
+    align-items: baseline;
+    outline: none;
+  }
 }
 </style>
