0cc49928cd8013d761f3124ad9132a4a3602bede
T. E. Shaw <t.e.shaw@dahoum.wales>
2020-09-29 19:13:28 +0200

    Update civilization.model.ts

--- common/models/civilization.model.ts
+++ common/models/civilization.model.ts
@@ -4,3 +4,9 @@
 # non of their contributions gets lost.
 #
 # Else they can destroy the civilisation or the project.
+#
+# The ownership of these contributions is re-assigned to the greenatlas.earth
+# Superuser.
+#
+# The Superuser can reassign them to interested parties (eg. other sites,
+# developers, owners, specialists.)
