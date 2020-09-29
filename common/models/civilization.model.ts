1d1725c05f0b3f47bd34b4175ddb1aaee8b37e63
T. E. Shaw <t.e.shaw@dahoum.wales>
2020-09-29 19:08:03 +0200

    Create civilization.model.ts

---
+++ common/models/civilization.model.ts
@@ -0,0 +1,6 @@
+# All contributions to a civilization belong to the civilization.
+#
+# If a contributor (owner, developer, specialist, etc.) leaves for whatever reason
+# non of their contributions gets lost.
+#
+# Else they can destroy the civilisation or the project.
