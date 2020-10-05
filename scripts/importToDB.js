0315d953916846e9a781f2aaf5bfa2983ba7d998
Peter Cat <peter@auxnederlandbv.nl>
2020-10-05 13:52:12 +0300

    add script for data import to FaunaDB

---
+++ scripts/importToDB.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const regdata = require('./mydata')
+
+const faunadb = require('faunadb')
+
+const q = faunadb.query
+
+const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET
+})
+
+async function go(len) {
+  for (let i = 0; i < len; i++) {
+    console.log(`im at ${i}th possition`)
+    try {
+      await client.query(
+        		  q.Create(
+        				q.Collection("region_fulldata"), { data: regdata[i]  }
+        			)
+      )
+    } catch (err) {
+      console.log(`OPPS: ${err}`)
+    }
+  }
+}
+
+go(regdata.length)
